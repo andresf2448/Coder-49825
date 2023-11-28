7bbcebac3b4f0136235a401af21a0decc8e89d8b
andresf2448 <andresf2448@gmail.com>
2023-11-27 19:59:45 -0500

    Clase 1

---
+++ Clase 1/main.js
@@ -0,0 +1,57 @@
+// comentario de linea
+
+/* 
+comentario
+de
+bloque
+*/
+
+//forma recomendada para declarar variables
+// let edad = 50; //variable de tipo number
+// let nombre = "andres"; //variable de tipo string
+
+//forma no recomandada para declarar variables
+// var direccion = "carrera 34 # 50 - 20";
+// var direccion = "AV";
+
+//constantes
+// const NUMERO = 5;
+
+// camelCase => nombreBonitoGrande
+// PascalCase => NombreBonitoGrande
+// snake_case => nombre_bonito_grande
+// kebabcase => nombre-bonito-grande
+
+// let edad = 70;
+
+// let numeroUno = 5;
+// let numeroDos = 20;
+
+// let resultadoSuma = numeroUno + numeroDos;
+// let resultadoResta = numeroUno - numeroDos;
+// let resultadoMultiplicacion = numeroUno * numeroDos;
+// let resultadoDivision = numeroUno / numeroDos;
+
+// console.log("resultado suma", resultadoSuma);
+// console.log("resultado resta", resultadoResta);
+// console.log("resultado multiplicacion", resultadoMultiplicacion);
+// console.log("resultado division", resultadoDivision);
+
+// let saludo = "Hola";
+// let nombre = "andres";
+
+// console.log(saludo + " " + nombre);
+
+// let nombre = prompt("Ingrese su nombre");
+// console.log(nombre);
+
+// let nombre = "pepito";
+// console.log(typeof nombre);
+
+// let numero = 5;
+// console.log(typeof numero);
+
+let numero = Number(prompt("Ingrese un numero"));
+// console.log(typeof numero);
+
+alert(numero + 3);
