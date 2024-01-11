7947204169cf5946401ed2cb497f417ad5897af2
andresf2448 <andresf2448@gmail.com>
2024-01-10 20:04:56 -0500

    Clase 11

--- Clase 11/main.js
+++ Clase 11/main.js
@@ -59,3 +59,96 @@
 
 // sessionStorage.removeItem("nombre"); //elimina la clave con su respectivo valor
 // sessionStorage.clear(); //elimina todo el localStorage
+
+//JSON
+//JSON.stringify(elemento); da formato json al elemento
+// const producto = { id: 1, nombre: "camisa", precio: 1000 };
+// const json = JSON.stringify(producto);
+
+// console.log(json);
+// console.log(typeof json);
+
+// const producto = { id: 1, nombre: "camisa", precio: 1000 };
+// localStorage.setItem("producto", JSON.stringify(producto));
+
+//JSON.parse(elemento); quitar el formato json al elemento
+// const producto = localStorage.getItem("producto");
+// console.log(JSON.parse(producto));
+
+// const productos = [
+//   { id: 1, nombre: "camisa", precio: 1000 },
+//   { id: 2, nombre: "media", precio: 750 },
+//   { id: 3, nombre: "zapato", precio: 980 },
+//   { id: 4, nombre: "gorra", precio: 320 },
+// ];
+
+// const guardarStorage = (clave, valor) => {
+//   localStorage.setItem(clave, valor);
+// };
+
+// productos.forEach((item) => {
+//   guardarStorage(item.id, JSON.stringify(item));
+// });
+
+// localStorage.setItem("carrito", JSON.stringify(productos));
+
+//////////////////////////////////////////////////////////////////
+// let usuario;
+//traemos el usuario del storage
+// let usuarioStorage = sessionStorage.getItem("usuario");
+
+//si hay usuario es porque es viejo
+// if (usuarioStorage) {
+//   usuario = usuarioStorage;
+//   alert(`Bienvenid@ nuevamente ${usuario}`);
+// } else {
+//   //cuando no hay usuario pedimos el usuario y guardamos en storage
+//   usuario = prompt("Ingrese el usuario");
+//   alert(`Bienvenid@ por primera vez ${usuario}`);
+//   sessionStorage.setItem("usuario", usuario);
+// }
+
+///////////////////////////////////////////////////////////////
+// const productos = [
+//   { id: 1, nombre: "camisa", precio: 1000 },
+//   { id: 2, nombre: "media", precio: 750 },
+//   { id: 3, nombre: "zapato", precio: 980 },
+//   { id: 4, nombre: "gorra", precio: 320 },
+// ];
+// localStorage.setItem("carrito", JSON.stringify(productos));
+
+let carrito;
+//traemos el carrito del storage
+let carritoStorage = localStorage.getItem("carrito");
+
+//traemos el boton para eliminar el carrito
+let boton = document.getElementById("boton");
+
+if (carritoStorage) {
+  carrito = JSON.parse(carritoStorage);
+} else {
+  carrito = [];
+  let div = document.createElement("div");
+  div.innerHTML = "Carrito vacío";
+
+  document.body.append(div);
+}
+
+//recorremos le carrito
+carrito.forEach((item) => {
+  let div = document.createElement("div");
+  div.innerHTML = `
+    <h2>Id: ${item.id}</h2>
+    <p>Nombre: ${item.nombre}</p>
+    <b>$${item.precio}</b>
+  `;
+
+  document.body.append(div);
+});
+
+//agreagamos el escuchador de evento para el boton
+boton.addEventListener("click", () => {
+  localStorage.clear(); //borramos el storage
+  alert("Carrito eliminado");
+  location.reload(); //recargamos la pagina
+});
