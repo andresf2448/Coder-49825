1605020003900a362965f61f3b1038977dfd8b04
andresf2448 <andresf2448@gmail.com>
2024-01-24 20:09:17 -0500

    Clase 15

---
+++ Clase 15/main.js
@@ -0,0 +1,140 @@
+//ASINCRONISMO
+//setTimeout(funcion, tiempo);
+
+// setTimeout(() => {
+//   console.log("Hola");
+// }, 3000);
+
+// console.log("inicio");
+
+// setTimeout(() => {
+//   console.log("Intermedio");
+// }, 3000);
+
+// console.log("Final");
+
+// let boton = document.getElementById("boton");
+// let saludo = document.getElementById("saludo");
+
+// boton.addEventListener("click", () => {
+//   saludo.classList.add("color");
+
+//   setTimeout(() => {
+//     saludo.classList.remove("color");
+//   }, 5000);
+// });
+
+// for (let letra of "Hola") {
+//   setTimeout(() => {
+//     console.log(letra);
+//   }, 1000);
+// }
+
+// for (let letra of "Mundo") {
+//   setTimeout(() => {
+//     console.log(letra);
+//   }, 3000);
+// }
+
+// console.log("inicio");
+
+// setTimeout(() => {
+//   console.log("Intermedio");
+// }, 0);
+
+// console.log("Final");
+
+// function multiply(x, y) {
+//   return x * y;
+// }
+
+// function printSquare(x) {
+//   let resultado = multiply(x, x);
+//   console.log(resultado);
+// }
+
+// printSquare(5);
+
+// console.log("inicio");
+
+// setTimeout(() => {
+//   console.log("Intermedio");
+// }, 0);
+
+// console.log("Final");
+
+// setTimeout(() => {
+//   console.log("Incio");
+// }, 5000);
+
+// setTimeout(() => {
+//   console.log("Intermedio");
+// }, 1000);
+
+//setInterval(funcion, tiempo);
+// setInterval(() => {
+//   console.log("Hola");
+// }, 1000);
+
+// let counter = 0;
+// const interval = setInterval(() => {
+//   counter++;
+//   console.log("Counter: ", counter);
+
+//   if (counter >= 5) {
+//     clearInterval(interval);
+//     console.log("Se removió el intervalo");
+//   }
+// }, 1000);
+
+//PROMESAS
+// new Promise((resolve, reject) => {
+//   //cuerpo de la promesa
+// });
+
+// const futuro = (valor) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       valor
+//         ? resolve(["camila", "andres", "carlito"])
+//         : reject("Promesa rechazada");
+//     }, 3000);
+//   });
+// };
+
+// futuro(true)
+//   .then((response) => console.log(response))
+//   .catch((error) => console.log(error))
+//   .finally(() => console.log("Proceso finalizado"));
+
+//////////////////////////////////////////////////////////////////////////////////////SERVIDOR
+const bd = [
+  { id: 1, nombre: "camisa", precio: 1000 },
+  { id: 2, nombre: "media", precio: 700 },
+  { id: 3, nombre: "zapato", precio: 800 },
+  { id: 4, nombre: "pantalon", precio: 1010 },
+];
+
+const pedirDatos = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(bd);
+    }, 3000);
+  });
+};
+//////////////////////////////////////////////////////////////////////////////////////
+
+pedirDatos()
+  .then((response) => {
+    response.forEach((item) => {
+      let div = document.createElement("div");
+      div.innerHTML = `
+        <h2>ID: ${item.id}</h2>
+        <p>Nombre: ${item.nombre}</p>
+        <b>$${item.precio}</b>
+      `;
+
+      document.body.append(div);
+    });
+  })
+  .catch((error) => {});
