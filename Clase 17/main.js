5199900d61dede49c23630a92ba0dc6228fc794b
andresf2448 <andresf2448@gmail.com>
2024-01-31 19:39:44 -0500

    Clase 17

---
+++ Clase 17/main.js
@@ -0,0 +1,10 @@
+// let mensaje = "Hola a todos";
+// console.log(mensaje);
+// console.log(mensaje);
+// console.log(mensaje);
+// console.log(mensaje);
+
+const { randomMsg } = require("random-messages-random");
+
+console.log(randomMsg());
+console.log(randomMsg());
